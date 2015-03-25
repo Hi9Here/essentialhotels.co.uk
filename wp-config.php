16d58c0e98e0fae4974266f688d6fc9951561d05
Wo <mywoisme@gmail.com>
2015-03-25 19:33:39 +0000

    changed wp-config to localhost

--- wp-config.php
+++ wp-config.php
@@ -33,7 +33,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'Zaq!2wsx');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
