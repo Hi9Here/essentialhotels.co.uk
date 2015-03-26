7338a098e1c6cd7538376aa15e5eb76de37f4566
Wo <mywoisme@gmail.com>
2015-03-26 16:45:17 +0000

    change just nav back while i sort out imports

--- wp-content/themes/listings-child/header.php
+++ wp-content/themes/listings-child/header.php
@@ -5,9 +5,9 @@
 <?php woo_meta(); ?>
 <?php global $woo_options; ?>
 
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
-<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed" />
-<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <script src="/bower_components/webcomponentsjs/webcomponents.min.js"></script>
     <link rel="import" href="/bower_components/paper-form-on-fire/paper-form-on-fire.html">
 
@@ -16,12 +16,10 @@
   	<link rel="import" href="elements/elements.html">
   	<link rel="stylesheet" href="app.css">
 
-
 	<!-- jQuery UI Datepicker - Animations   -->
     <link rel="stylesheet" href="//code.jquery.com/ui/1.9.1/themes/base/jquery-ui.css"/>
 	<script src="//code.jquery.com/jquery-1.8.2.js"></script>
     <script src="//code.jquery.com/ui/1.9.1/jquery-ui.js"></script>
-
     <link rel="stylesheet" href="/resources/demos/style.css" />
 
 <?php wp_head(); ?>
@@ -57,59 +55,30 @@
 </div>
 	
 <div id="navig">
-	<body unresolved fullbleed layout vertical>
+		<?php
+		if ( function_exists('has_nav_menu') && has_nav_menu('primary-menu') ) {
+			wp_nav_menu( array( 'depth' => 6, 'sort_column' => 'menu_order', 'container' => 'ul', 'menu_id' => 'main-nav', 'menu_class' => 'nav fl', 'theme_location' => 'primary-menu' ) );
+		} else {
+		?>
+        <ul id="main-nav" class="nav fl">
+			<?php 
+        	if ( isset($woo_options['woo_custom_nav_menu']) AND $woo_options['woo_custom_nav_menu'] == 'true' ) {
+        		if ( function_exists('woo_custom_navigation_output') )
+					woo_custom_navigation_output();
 
-	<core-drawer-panel id="drawerPanel" responsiveWidth="1000px">
+			} else { ?>
+            	
+	            <?php if ( is_page() ) $highlight = "page_item"; else $highlight = "page_item current_page_item"; ?>
+	            <li class="<?php echo $highlight; ?>"><a href="/"><?php _e('Home', 'woothemes') ?></a></li>
+	            <?php 
+	    			wp_list_pages('sort_column=menu_order&depth=6&title_li=&exclude='.get_option('woo_listings_cpt_page')); 
 
-	  <core-header-panel drawer>
-	    <core-toolbar icon="menu">Menu</core-toolbar>
-	    <core-menu selected="0">
-	      <core-item icon="home" label="Home"><a href="https://essentialhotels.co.uk"></a></core-item>
-	      <core-item icon="maps:hotel" label="All destinations"><a href="https://www.essentialhotels.co.uk/f.html#/hotels/World/"></a></core-item>
-	      <core-item icon="maps:hotel" label="London"><a href="https://www.essentialhotels.co.uk/f.html#/hotels/London/"></a></core-item>
-	      <core-item icon="maps:hotel" label="Manchester"><a href="https://www.essentialhotels.co.uk/f.html#/hotels/Manchester/"></a></core-item>
-	      <core-item icon="maps:hotel" label="Paris"><a href="https://essentialhotels.co.uk/f.html#/hotels/Paris/"></a></core-item>
-	      <core-item icon="maps:hotel" label="Brighton"><a href="https://essentialhotels.co.uk/f.html#/hotels/Brighton/"></a></core-item>
-	      <core-item icon="maps:hotel" label="Rome"><a href="https://essentialhotels.co.uk/f.html#/hotels/Rome/"></a></core-item>
-	      <core-item icon="maps:hotel" label="Madrid"><a href="https://essentialhotels.co.uk/f.html#/hotels/Madrid/"></a></core-item>
-	      <core-item icon="maps:hotel" label="New York"><a href="https://essentialhotels.co.uk/f.html#/hotels/New%20York/"></a></core-item>
-	      <core-item icon="maps:hotel" label="Dubai"><a href="https://essentialhotels.co.uk/f.html#/hotels/Dubai/"></a></core-item>
-	    </core-menu>
-	  </core-header-panel>
-
-	  <core-header-panel main>
-	    <core-toolbar>
-	      <paper-icon-button id="navicon" icon="menu" core-drawer-toggle></paper-icon-button>
-	      <div><img src="elements/EH-Logo.png" alt="Essential Hotels" style="width: 76px;"></div>
-	      <span flex></span>
-		    <div style="width: 40%;">
-		      <script>
-				  (function() {
-				    var cx = '012477433197051517293:sl1ssjlfhme';
-				    var gcse = document.createElement('script');
-				    gcse.type = 'text/javascript';
-				    gcse.async = true;
-				    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-				        '//www.google.com/cse/cse.js?cx=' + cx;
-				    var s = document.getElementsByTagName('script')[0];
-				    s.parentNode.insertBefore(gcse, s);
-				  })();
-				</script>
-				<gcse:search></gcse:search>
-			</div>
-          <paper-icon-button icon="communication:call"><a href="tel:+44-0800-180-4700"></a></paper-icon-button>
-          <!-- <paper-icon-button icon="search"></paper-icon-button> -->
-		  <div>Enquiry</div>
-	    </core-toolbar>
-	  </core-header-panel>
-
-	</core-drawer-panel>
-
-
-</body>
-
-
-</div><!-- /#navigation -->
+			}
+			?>
+        </ul><!-- /#nav -->
+        <?php } ?>
+        
+	</div><!-- /#navigation -->
 
 	</div><!-- /#header -->
 
